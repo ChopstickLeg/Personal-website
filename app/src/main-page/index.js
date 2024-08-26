d6d305100c474b32ccec27479762c01921909874
Hayden Carpenter <hayden.carpenter@amwins.com>
2024-08-26 15:00:19 -0400

    Changed version of react-router-dom and added ability to call github api to see my public repos

--- app/src/main-page/index.js
+++ app/src/main-page/index.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Octokit } from "octokit";
 import "./main-page.css";
 import Header from "./header";
 
 function App() {
-  //variable to store list of projects, won't be cleared on rerender
-  //inside const brackets, place <list name>, <setter function name from end of useEffect>
-  const [] = useState([]);
+  // eslint-disable-next-line
+  const [repos, setRepos] = useState([]);
   useEffect(() => {
-    //logic for fetching GitHub Projects
-    //this will only run on initial pageload since brackets are empty
+    const getRepos = async () => {
+      const octokit = new Octokit({});
+      const gitRepos = await octokit.paginate("GET /users/{username}/repos", {
+        username: "ChopstickLeg",
+      });
+      setRepos(gitRepos);
+      console.log(gitRepos);
+    };
+    getRepos();
   }, []);
   return (
     <Router>
       <div className="container">
         <Header subtitle="Software Developer Hayden Carpenter's Website" />
-
         <Switch>
           <Route path="/"></Route>
         </Switch>
