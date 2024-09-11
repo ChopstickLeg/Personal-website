1799216711537a272d8b3caab8376630be3598de
Hayden Carpenter <hayden.carpenter@amwins.com>
2024-09-11 16:12:44 -0400

    messing with divs on navbar

--- app/src/main-page/navbar.js
+++ app/src/main-page/navbar.js
@@ -3,9 +3,14 @@ import { NavLink as Link } from "react-router-dom";
 const NavigationBar = () => {
   return (
     <div className="col-md-12 text-center">
-      <Link to="/about">About me&emsp;</Link>
-
-      <Link to="/projects">My Projects</Link>
+      <div className="d-flex justify-content-center align-items-center">
+        <div>
+          <Link to="/about">About me</Link>
+        </div>
+        <div>
+          <Link to="/projects">My Projects</Link>
+        </div>
+      </div>
     </div>
   );
 };
